532a2ece044cd9fee23b647eac97906196699557
Bryan Wu <bwu@bandwidth.com>
2020-06-12 01:32:16 -0400

    move away from class into pure exports

--- testing/playground.js
+++ testing/playground.js
@@ -1,5 +1,11 @@
 //A Free-for-all playground to run code without changing things, hopefully.
-const { Api } = require('../api-utils');
+
+
+
+/*const { Api } = require('../api-utils');
 console.log(Api);
 const api = new Api();
 api.listSippeers(35593);
+*/
+
+console.log(module.exports)
