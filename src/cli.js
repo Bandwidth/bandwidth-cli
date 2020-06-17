4c100f83cbacede8ca1f228f9d5c9b8295e55fbd
Bryan Wu <bwu@bandwidth.com>
2020-06-17 18:29:46 -0400

    use bandwidth iris sdk

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,9 @@
 const { Command } = require('commander');
+const utils = require('./api-utils')
+var numbers = require("@bandwidth/numbers");
+numbers.Client.globalOptions.accountId = process.env.BANDWIDTH_ACCOUNT_ID;
+numbers.Client.globalOptions.userName = process.env.BANDWIDTH_API_USER;
+numbers.Client.globalOptions.password = process.env.BANDWIDTH_API_PASSWORD;
 module.exports.program = program = new Command();
 
 const description = 'A descriptive description to describe something worth describing with a description.';
@@ -8,16 +13,42 @@ program
 
 /**************************'CREATE' COMMAND**************************/
 
-const createCmd = program.command('create');
+const createCmd = program.command('create')
+  .alias('c')
 
-const createAppCmd = createCmd.command('app')
+const createAppCmd = createCmd.command('app <name>')
+  .alias('a')
+  .requiredOption('-t, --type <type>', 'An application must be a voice(v) or messaging(m) application')
+  .action((name, cmdObj) => {
+    const options = cmdObj.opts();
+    switch (options.type) {
+      case 'v':
+      case 'voice':
+        {
+          utils.createVoiceApplication({
+            name: name
+          })
+        }
+        break;
+      case 'm':
+      case 'messaging':
+        {
+          console.log('creating messaging app')
+        }
+        break;
+      default:
+        console.log('type must be either voice(v) or messaging(m)')
+    }
+  })
 
-createAppCmd.action(console.log('create app command invoked'))
+
+
 
 /**************************'LIST' COMMAND**************************/
 
 const listCmd = program.command('list');
-
+const listAppCmd = listCmd.command('app <name>')
+  .alias('a')
 
 /**************************'DELETE' COMMAND**************************/
 
