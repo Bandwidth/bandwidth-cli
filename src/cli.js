543585f40401a6de041e70eac9efad0e1add4f7f
Bryan Wu <bwu@bandwidth.com>
2020-06-17 18:45:10 -0400

    functional site listing using sdk

--- src/cli.js
+++ src/cli.js
@@ -39,25 +39,49 @@ const createAppCmd = createCmd.command('app <name>')
     }
   })
 
-  const createSiteCmd = createCmd.command('site <name>')
-    .alias('s')
-    .action((name, cmdObj) => {
-      const options = cmdObj.opts();
-    })
+const createSiteCmd = createCmd.command('site <name>')
+  .alias('s')
+  .action((name, cmdObj) => {
+    const options = cmdObj.opts();
+  })
 
-  const createSippeerCmd = createCmd.command('sipper <name>')
-    .alias('s')
-    .alias('sip')
-    .action((name, cmdObj) => {
-      const options = cmdObj.opts();
-    })
+const createSippeerCmd = createCmd.command('sipper <name>')
+  .alias('p')
+  .alias('sip')
+  .action((name, cmdObj) => {
+    const options = cmdObj.opts();
+  })
 
 
 /**************************'LIST' COMMAND**************************/
 
-const listCmd = program.command('list');
-const listAppCmd = listCmd.command('app <name>')
+const listCmd = program.command('list')
+  .alias('l');
+const listAppCmd = listCmd.command('app')
   .alias('a')
+  .alias('apps')
+  .action(async () => {
+
+    //list things
+  })
+const listSiteCmd = listCmd.command('site')
+  .alias('s')
+  .alias('sites')
+  .action(async () => {
+    try {
+      const sitesList = await numbers.Site.listAsync()
+      console.log(sitesList)
+    } catch {
+      console.log('an error has occured')
+    }
+    //list things
+  })
+const listSippeerCmd = listCmd.command('sippeer')
+  .alias('p')
+  .alias('sippeers')
+  .action(async () => {
+    //list things
+  })
 
 /**************************'DELETE' COMMAND**************************/
 
