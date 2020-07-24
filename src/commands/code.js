fdc22a84b64acbb4a9433540f3da4ad3ef45b412
Bryan Wu <bwu@bandwidth.com>
2020-07-24 17:05:48 -0400

    add replacement of app id

--- src/commands/code.js
+++ src/commands/code.js
@@ -15,5 +15,13 @@ module.exports.codeCallbackServerAction = async (cmdObj) => {
     throw new BadInputError(`The destination folder, ${destPath}, alraedy exists.`, 'out', 'Write to a new directory, or use --force to overwrite the existing directory.')
   }
   copydir.sync(sourcePath, destPath)
-  printer.success('Server generated successfully. To ')
+  printer.success(`Server generated successfully. Find the it in ${destPath}`)
+  const indexFileLocation = path.join(destPath, `index.js`) //TODO add support for other languages
+  const defaultAppId = await utils.readDefault('application');
+  if (defaultAppId){
+    let result = fs.readFileSync(indexFileLocation);
+    result = result.toString();
+    result = result.replace(/INSERT YOUR MESSAGING APPLICATION ID HERE./g, defaultAppId);
+    fs.writeFileSync(indexFileLocation, result)
+  }
 }
