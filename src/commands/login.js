897e3d9967fc0885c7030055baf60286253d79cf
Bryan Wu <bwu@bandwidth.com>
2020-07-30 17:51:30 -0400

    Immediate account credentials check on logging in

--- src/commands/login.js
+++ src/commands/login.js
@@ -1,6 +1,7 @@
 const printer = require('../printer');
 const utils = require('../utils');
 const { BadInputError } = require('../errors');
+const numbers = require("@bandwidth/numbers");
 
 module.exports.loginAction = async () => {
   printer.print('Leaving a field blank will keep it at its previous value.')
@@ -8,6 +9,12 @@ module.exports.loginAction = async () => {
   if (!(username || password || accountId)) {
     return printer.warn('No credentials were entered and the login has been aborted.')
   }
+  const client = new numbers.Client(accountId, username, password);
+  await numbers.Account.getAsync(client).catch((err) => {
+    if (err.status === 401) {
+      throw new BadInputError('Account authentication failed and your credentials have not been saved. Please try again.')
+    }
+  })
   await utils.saveAccountId(accountId);
   if (!await utils.readAccountId()) {
     throw new BadInputError('An account ID is required if none is currently set.');
